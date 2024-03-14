da58f3aa96ae3e0db9bdaf53fcfaacfdd1a6d91f
Isabella Saenz <145500885+IsabellaSAENZ@users.noreply.github.com>
2024-03-14 01:22:08 +0000

    event listener
    added content to the second button in an unordered list with terms learned

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function myFunction(){
  document.getElementById("title").style.textTransform = "uppercase"; 
 }
+
+let button = document.getElementById("btn-2");
+console.log(button);
+let termUl = document.getElementById("btn-2-list");
+
+let terms = ["branch", "commit", "push"];
+
+button.addEventListener("click", (e) => {
+  console.log("e: ", e);
+  for (let i = 0; i < terms.length; i++){
+    let currentLi = document.createElement("li");
+    currentLi.textContent = terms[i];
+    console.log(currentLi);
+    termUl.appendChild(currentLi);
+  }
+})
